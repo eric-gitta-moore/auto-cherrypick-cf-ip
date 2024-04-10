c529577f27f6dfee485220012365455018dc8bf2
Eric Moore <EricGittaMoore@duck.com>
2024-04-10 23:16:39 +0800

    init

--- src/adapter/CloudFlareYes/index.ts
+++ src/adapter/CloudFlareYes/index.ts
@@ -4,13 +4,6 @@ export default function () {
       accept: "application/json, text/plain, */*",
       "accept-language": "en",
       "content-type": "application/json",
-      "sec-ch-ua":
-        '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": '"Windows"',
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-site",
       Referer: "https://stock.hostmonit.com/",
       "Referrer-Policy": "strict-origin-when-cross-origin",
     },
